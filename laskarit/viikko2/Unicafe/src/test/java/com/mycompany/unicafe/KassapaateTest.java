599f27b4d3a3003dd8eac1d6e54efa7e4004ea8f
Tero Vuori <tero.vuori@outlook.com>
2020-03-20 08:35:27 +0200

    unicafe testikattavuus

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/KassapaateTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/KassapaateTest.java
@@ -26,7 +26,7 @@ public class KassapaateTest {
     @Before
     public void setUp() {
         kassa=new Kassapaate();
-        kortti = new Maksukortti(10);
+        kortti = new Maksukortti(1000);
     }
     
     @After
@@ -41,6 +41,16 @@ public class KassapaateTest {
     @Test
     public void luodussaKassassaOikeaSaldo() {
         assertEquals(100000, kassa.kassassaRahaa());
+    }
+    
+    @Test
+    public void luodussaKassassaOikeaMaaraEdullisiaLounaita() {
+        assertEquals(0, kassa.edullisiaLounaitaMyyty());
+    }
+    
+    @Test
+    public void luodussaKassassaOikeaMaaraMaukkaitaLounaita() {
+        assertEquals(0, kassa.maukkaitaLounaitaMyyty());
     }
     
     @Test
@@ -63,8 +73,114 @@ public class KassapaateTest {
     
     @Test
     public void syoEdullisestiEiOnnistuJosLiianVahanRahaa() {
-        kassa.syoEdullisesti(5);
+        assertEquals(5, kassa.syoEdullisesti(5));
         assertEquals(0, kassa.edullisiaLounaitaMyyty());
-        
     }
+    
+    @Test
+    public void syoMaukkaastiToimii() {
+        kassa.syoMaukkaasti(400);
+        assertEquals(100400, kassa.kassassaRahaa());
+    }
+    
+    @Test
+    public void syoMaukkaastiPalauttaaOikein() {
+        assertEquals(100, kassa.syoMaukkaasti(500));
+        assertEquals(100400, kassa.kassassaRahaa());
+    }
+    
+    @Test
+    public void syoMaukkaastiLisaaMyytyjaMaukkaita() {
+        kassa.syoMaukkaasti(500);
+        assertEquals(1, kassa.maukkaitaLounaitaMyyty());
+    }
+    
+    @Test
+    public void syoMaukkaastiEiOnnistuJosLiianVahanRahaa() {
+        assertEquals(5, kassa.syoMaukkaasti(5));
+        assertEquals(0, kassa.maukkaitaLounaitaMyyty());
+    }
+    
+    @Test
+    public void syoMaukkaastiKortillaToimii() {
+        kassa.syoMaukkaasti(kortti);
+        assertEquals(100000, kassa.kassassaRahaa());
+        assertEquals(600, kortti.saldo());
+    }
+    
+    @Test
+    public void syoMaukkaastiKortillaPalauttaaOikein() {
+        assertEquals(true, kassa.syoMaukkaasti(kortti));
+        assertEquals(100000, kassa.kassassaRahaa());
+        assertEquals(600, kortti.saldo());
+    }
+    
+    @Test
+    public void syoMaukkaastiKortillaLisaaMyytyjaMaukkaita() {
+        kassa.syoMaukkaasti(kortti);
+        assertEquals(1, kassa.maukkaitaLounaitaMyyty());
+    }
+    
+    @Test
+    public void syoMaukkaastiKortillaEiOnnistuJosLiianVahanRahaa() {
+        kortti.otaRahaa(955);
+        assertEquals(false, kassa.syoMaukkaasti(kortti));
+        assertEquals(0, kassa.maukkaitaLounaitaMyyty());
+    }
+    
+    @Test
+    public void syoEdullisestiKortillaToimii() {
+        kassa.syoEdullisesti(kortti);
+        assertEquals(100000, kassa.kassassaRahaa());
+        assertEquals(760, kortti.saldo());
+    }
+    
+    @Test
+    public void syoEdullisestiKortillaPalauttaaOikein() {
+        assertEquals(true, kassa.syoEdullisesti(kortti));
+        assertEquals(100000, kassa.kassassaRahaa());
+        assertEquals(760, kortti.saldo());
+    }
+    
+    @Test
+    public void syoEdullisestiKortillaLisaaMyytyjaMaukkaita() {
+        kassa.syoEdullisesti(kortti);
+        assertEquals(1, kassa.edullisiaLounaitaMyyty());
+    }
+    
+    @Test
+    public void syoEdullisestiKortillaEiOnnistuJosLiianVahanRahaa() {
+        kortti.otaRahaa(955);
+        assertEquals(false, kassa.syoEdullisesti(kortti));
+        assertEquals(0, kassa.edullisiaLounaitaMyyty());
+    }
+    
+    @Test
+    public void lataaRahaaToimiiJosNegatiivinen() {
+        kortti.lataaRahaa(-5);
+        assertEquals(995,kortti.saldo());
+        assertEquals(100000, kassa.kassassaRahaa());
+    }
+    
+    @Test
+    public void lataaRahaaToimiiJosPositiivinen() {
+        kortti.lataaRahaa(5);
+        assertEquals(1005,kortti.saldo());
+        assertEquals(100000, kassa.kassassaRahaa());
+    }
+    
+    @Test
+    public void lataaRahaaKortillaToimiiJosNegatiivinen() {
+        kassa.lataaRahaaKortille(kortti, -5);
+        assertEquals(1000,kortti.saldo());
+        assertEquals(100000, kassa.kassassaRahaa());
+    }
+    
+    @Test
+    public void lataaRahaaKortillaToimiiJosPositiivinen() {
+        kassa.lataaRahaaKortille(kortti, 5);
+        assertEquals(1005,kortti.saldo());
+        assertEquals(100005, kassa.kassassaRahaa());
+    }
+    
 }
